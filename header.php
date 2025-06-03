d830479e76ffb7d5a32b9f0f565367cebd914f36
jonathanmari <jonathan_mari@hotmail.fr>
2025-06-03 11:17:18 +0200

    ajout de la navbar responsive (liens non actifs encore)

--- header.php
+++ header.php
@@ -1,8 +1,39 @@
 <?php
-    echo 
-    '<header class="container" id="header">
-        <div class="col-lg-12 text-center">
-            <h1 id="site-title">Crunch & Munch</h1>
+    echo <<<HTML
+    
+    <nav class="navbar navbar-dark bg-dark navbar-expand-lg">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Munch & Crunch</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#navbarOffcanvasLg" aria-controls="navbarOffcanvasLg" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="offcanvas offcanvas-start text-bg-dark" tabindex="-1" id="navbarOffcanvasLg" aria-labelledby="navbarOffcanvasLgLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title">Munch & Crunch</h5>
+                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Toutes les recettes</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Tous les programmes</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Tous les exercices
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Triceps</a></li>
+                                <li><a class="dropdown-item" href="#">Biceps</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
-    </header>';
+    </nav>
+
+    HTML;
 ?>
