958a176aa27d6811b12b58e091d1681460517d45
jonathanmari <jonathan_mari@hotmail.fr>
2025-06-18 11:30:10 +0200

    création du dashboard; renvoi vers index si pas connecté

---
+++ dashboard.php
@@ -0,0 +1,52 @@
+<?php 
+    session_start();
+    if($_SESSION['user_id'] == ''):
+        header('Location: index.php');
+    endif;
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crunch & Munch</title>
+    <script src="script.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <link href="style.css" rel="stylesheet">
+</head>
+
+<body>
+    <?php include 'header.php'; ?>
+
+    <?php if (!empty($successMsg)): ?>
+        <div class="toast-container position-fixed top-50 start-50 translate-middle p-3">
+            <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+                <div class="toast-header">
+                    <img src="" class="rounded me-2" alt="...">
+                    <strong class="me-auto">Info de connexion</strong>
+                    <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+                </div>
+                <div class="toast-body">
+                    <?php echo $successMsg; ?>
+                </div>
+            </div>
+        </div>
+    <?php $_SESSION['successMsg'] = NULL; endif; ?>
+
+    <main>
+
+    </main>
+
+    <footer class="bg-dark text-white text-center py-3">
+        <?php include 'footer.php'; ?>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
+        crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
